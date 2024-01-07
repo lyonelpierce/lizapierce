a1e6b40f7b7e8a486b279745d53070bc196959ad
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2024-01-06 21:35:57 -0500

    sidebar

--- components/CustomizeForm.tsx
+++ components/CustomizeForm.tsx
@@ -33,6 +33,7 @@ import { CartProduct } from "@/types/ProductWithVariants";
 import CanvasComponent from "@/components/CanvasComponent";
 import ElizabethRing from "@/components/ElizabethRing";
 import { Gem, Karat, Material, Product, Size } from "@prisma/client";
+import { Separator } from "./ui/separator";
 
 const CustomizeForm = ({
   product,
@@ -128,167 +129,40 @@ const CustomizeForm = ({
           />
         </CanvasComponent>
       </div>
-      <div className="flex flex-col gap-4 w-1/3">
-        <div className="font-normal">
-          <h2 className="text-3xl">{product.name}</h2>
-          <h3 className="text-xl">{formatter.format(price)}</h3>
-        </div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="size"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Size</FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      defaultValue={field.value}
-                      onValueChange={field.onChange}
-                      className="flex"
-                    >
-                      {sizes.map((size) => (
-                        <div className="flex items-center" key={size.id}>
-                          <RadioGroupItem
-                            value={size.value}
-                            id={size.id}
-                            className="peer sr-only"
-                          />
-                          <Label
-                            htmlFor={size.id}
-                            className="bg-zinc-800 rounded-full w-16 h-10 py-2.5 flex items-center justify-center transition-all ease-in-out hover:scale-105 hover:bg-zinc-700 cursor-pointer hover:border-zinc-200 peer-data-[state=checked]:bg-zinc-600 [&:has([data-state=checked])]:bg-zinc-600"
-                          >
-                            <p className="text-xs font-semibold">{size.name}</p>
-                          </Label>
-                        </div>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="gem"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Gem</FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      defaultValue={field.value}
-                      onValueChange={field.onChange}
-                      className="flex"
-                    >
-                      {gems.map((gem) => (
-                        <div className="flex items-center" key={gem.id}>
-                          <RadioGroupItem
-                            value={gem.value}
-                            id={gem.id}
-                            onClick={() => setGem(gem.value)}
-                            className="peer sr-only"
-                          />
-                          <Label
-                            htmlFor={gem.id}
-                            className="bg-zinc-800 rounded-full h-10 py-2.5 px-4 flex items-center justify-center transition-all ease-in-out hover:scale-105 hover:bg-zinc-700 cursor-pointer hover:border-zinc-200 peer-data-[state=checked]:bg-zinc-600 [&:has([data-state=checked])]:bg-zinc-600"
-                          >
-                            <p className="flex gap-1 items-center text-xs font-semibold">
-                              <Image
-                                src={gem.image}
-                                alt={gem.name}
-                                width={20}
-                                height={20}
-                              />
-                              {gem.name}
-                            </p>
-                          </Label>
-                        </div>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="material"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Material</FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      defaultValue={field.value}
-                      onValueChange={field.onChange}
-                      className="flex"
-                    >
-                      {materials.map((material) => (
-                        <div className="flex items-center" key={material.id}>
-                          <RadioGroupItem
-                            value={material.value}
-                            id={material.id}
-                            onClick={() => setMaterial(material.value)}
-                            className="peer sr-only"
-                          />
-                          <Label
-                            htmlFor={material.id}
-                            className="bg-zinc-800 rounded-full h-10 py-2.5 px-4 flex gap-1 items-center justify-center transition-all ease-in-out hover:scale-105 hover:bg-zinc-700 cursor-pointer hover:border-zinc-200 peer-data-[state=checked]:bg-zinc-600 [&:has([data-state=checked])]:bg-zinc-600"
-                          >
-                            <div
-                              className={cn(
-                                "w-6 h-6 rounded-full bg-gradient-to-tr flex-none",
-                                {
-                                  "from-gray-500 via-white to-gray-500":
-                                    material.value === "platinum",
-                                  "from-gray-700 via-white to-gray-700":
-                                    material.value === "white-gold",
-                                  "from-rose-300 via-white to-rose-300":
-                                    material.value === "rose-gold",
-                                  "from-amber-500 via-white to-amber-500":
-                                    material.value === "gold",
-                                  "from-gray-900 via-white to gray-900":
-                                    material.value === "silver",
-                                }
-                              )}
-                            />
-                            <p className="text-xs font-semibold">
-                              {material.name}
-                            </p>
-                          </Label>
-                        </div>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            {(material === "gold" ||
-              material === "rose-gold" ||
-              material === "white-gold") && (
+      <div className="flex flex-col justify-between gap-4 w-1/3 border bg-zinc-950 border-zinc-800 rounded-lg p-6">
+        <div className="space-y-4">
+          <div className="font-normal">
+            <h2 className="text-3xl">{product.name}</h2>
+            <h3 className="text-xl">{formatter.format(price)}</h3>
+          </div>
+          <Separator className="bg-zinc-800" />
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
               <FormField
                 control={form.control}
-                name="karat"
+                name="size"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Karat</FormLabel>
+                    <FormLabel>Size</FormLabel>
                     <FormControl>
                       <RadioGroup
                         defaultValue={field.value}
                         onValueChange={field.onChange}
-                        className="flex"
+                        className="flex gap-3"
                       >
-                        {karats.map((karat) => (
-                          <div className="flex items-center" key={karat.id}>
+                        {sizes.map((size) => (
+                          <div className="flex items-center" key={size.id}>
                             <RadioGroupItem
-                              value={karat.value}
-                              id={karat.id}
-                              onClick={() => setKarat(karat.value)}
+                              value={size.value}
+                              id={size.id}
                               className="peer sr-only"
                             />
                             <Label
-                              htmlFor={karat.id}
-                              className="bg-zinc-800 rounded-full w-16 h-10 py-2.5 flex items-center justify-center transition-all ease-in-out hover:scale-105 hover:bg-zinc-700 cursor-pointer hover:border-zinc-200 peer-data-[state=checked]:bg-zinc-600 [&:has([data-state=checked])]:bg-zinc-600"
+                              htmlFor={size.id}
+                              className="bg-zinc-900 border border-zinc-800 rounded-full px-4 h-8 flex items-center justify-center transition-all ease-in-out hover:scale-110 cursor-pointer hover:border-white peer-data-[state=checked]:border-white [&:has([data-state=checked])]:border-white"
                             >
                               <p className="text-xs font-semibold">
-                                {karat.name}
+                                {size.name}
                               </p>
                             </Label>
                           </div>
@@ -298,14 +172,146 @@ const CustomizeForm = ({
                   </FormItem>
                 )}
               />
-            )}
-            <Button type="submit" className="gap-1 w-full" variant="white">
-              <ShoppingBag className="w-4 h-4" />
-              Add to cart
-            </Button>
-          </form>
-        </Form>
-        <div className="space-y-4 border border-zinc-800 rounded-md p-4">
+              <FormField
+                control={form.control}
+                name="gem"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Gem</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                        className="flex"
+                      >
+                        {gems.map((gem) => (
+                          <div className="flex items-center" key={gem.id}>
+                            <RadioGroupItem
+                              value={gem.value}
+                              id={gem.id}
+                              onClick={() => setGem(gem.value)}
+                              className="peer sr-only"
+                            />
+                            <Label
+                              htmlFor={gem.id}
+                              className="bg-zinc-900 border border-zinc-800 rounded-full px-4 h-8 flex items-center justify-center transition-all ease-in-out hover:scale-110 cursor-pointer hover:border-white peer-data-[state=checked]:border-white [&:has([data-state=checked])]:border-white"
+                            >
+                              <p className="flex gap-1 items-center text-xs font-semibold">
+                                <Image
+                                  src={gem.image}
+                                  alt={gem.name}
+                                  width={18}
+                                  height={18}
+                                />
+                                {gem.name}
+                              </p>
+                            </Label>
+                          </div>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="material"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Material</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                        className="flex"
+                      >
+                        {materials.map((material) => (
+                          <div className="flex items-center" key={material.id}>
+                            <RadioGroupItem
+                              value={material.value}
+                              id={material.id}
+                              onClick={() => setMaterial(material.value)}
+                              className="peer sr-only"
+                            />
+                            <Label
+                              htmlFor={material.id}
+                              className="bg-zinc-900 border border-zinc-800 rounded-full px-4 h-8 flex gap-1 items-center justify-center transition-all ease-in-out hover:scale-110 cursor-pointer hover:border-white peer-data-[state=checked]:border-white [&:has([data-state=checked])]:border-white"
+                            >
+                              <div
+                                className={cn(
+                                  "w-5 h-5 rounded-full bg-gradient-to-tr flex-none",
+                                  {
+                                    "from-gray-500 via-white to-gray-500":
+                                      material.value === "platinum",
+                                    "from-gray-700 via-white to-gray-700":
+                                      material.value === "white-gold",
+                                    "from-rose-300 via-white to-rose-300":
+                                      material.value === "rose-gold",
+                                    "from-amber-500 via-white to-amber-500":
+                                      material.value === "gold",
+                                    "from-gray-900 via-white to gray-900":
+                                      material.value === "silver",
+                                  }
+                                )}
+                              />
+                              <p className="text-xs font-semibold">
+                                {material.name}
+                              </p>
+                            </Label>
+                          </div>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              {(material === "gold" ||
+                material === "rose-gold" ||
+                material === "white-gold") && (
+                <FormField
+                  control={form.control}
+                  name="karat"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Karat</FormLabel>
+                      <FormControl>
+                        <RadioGroup
+                          defaultValue={field.value}
+                          onValueChange={field.onChange}
+                          className="flex"
+                        >
+                          {karats.map((karat) => (
+                            <div className="flex items-center" key={karat.id}>
+                              <RadioGroupItem
+                                value={karat.value}
+                                id={karat.id}
+                                onClick={() => setKarat(karat.value)}
+                                className="peer sr-only"
+                              />
+                              <Label
+                                htmlFor={karat.id}
+                                className="bg-zinc-900 border border-zinc-800 rounded-full px-4 h-8 flex items-center justify-center transition-all ease-in-out hover:scale-110 cursor-pointer hover:border-white peer-data-[state=checked]:border-white [&:has([data-state=checked])]:border-white"
+                              >
+                                <p className="text-xs font-semibold">
+                                  {karat.name}
+                                </p>
+                              </Label>
+                            </div>
+                          ))}
+                        </RadioGroup>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              )}
+              <Button type="submit" className="gap-1 w-full" variant="white">
+                <ShoppingBag className="w-4 h-4" />
+                Add to cart
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 bg-black rounded-md p-4">
           <h4 className="text-xs text-center">Guaranteed Safe Checkout</h4>
           <div className="flex items-center justify-center gap-4">
             <FaCcStripe className="w-10 h-10" />
