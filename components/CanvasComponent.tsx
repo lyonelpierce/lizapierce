0021b9bc527b6a29a35a58dc527397dd4e0c5398
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2024-01-09 18:40:01 -0500

    loader

--- components/CanvasComponent.tsx
+++ components/CanvasComponent.tsx
@@ -3,6 +3,7 @@
 import { Canvas } from "@react-three/fiber";
 import {
   Environment,
+  Loader,
   OrbitControls,
   PresentationControls,
 } from "@react-three/drei";
@@ -10,6 +11,22 @@ import { Suspense } from "react";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 import { cn } from "@/lib/utils";
+
+const containerStyles = {
+  backgroundColor: "black",
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const textStyles = {
+  color: "white",
+  fontSize: "0.75rem",
+  fontWeight: 500,
+  textTransform: "uppercase",
+};
 
 const CanvasComponent = ({
   children,
@@ -23,40 +40,43 @@ const CanvasComponent = ({
   intensity: number;
 }) => {
   return (
-    <Canvas
-      camera={{ position: [5, 10, 5], fov: 45 }}
-      className={cn("w-full z-10", className)}
-      gl={{ antialias: true }}
-    >
-      <ambientLight intensity={0.5} />
-      <spotLight position={[5, 5, -10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <PresentationControls
-        polar={[-Math.PI / 3, Math.PI / 3]}
-        azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+    <>
+      <Canvas
+        camera={{ position: [5, 10, 5], fov: 45 }}
+        className={cn("w-full z-10", className)}
+        gl={{ antialias: true }}
       >
-        <Suspense fallback={null}>{children}</Suspense>
-      </PresentationControls>
-      <Environment files="https://demo-assets.pixotronics.com/pixo/presets/environment/env-metal-1.hdr" />
-      <OrbitControls
-        makeDefault
-        autoRotate
-        autoRotateSpeed={3}
-        minPolarAngle={0}
-        maxPolarAngle={Math.PI / 2}
-        enableZoom={false}
-        enablePan={false}
-      />
-      <EffectComposer multisampling={0} disableNormalPass={true}>
-        <Bloom
-          luminanceThreshold={1}
-          luminanceSmoothing={0.9}
-          intensity={intensity}
-          levels={level}
-          mipmapBlur
+        <ambientLight intensity={0.5} />
+        <spotLight position={[5, 5, -10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <PresentationControls
+          polar={[-Math.PI / 3, Math.PI / 3]}
+          azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+        >
+          <Suspense fallback={null}>{children}</Suspense>
+        </PresentationControls>
+        <Environment files="https://demo-assets.pixotronics.com/pixo/presets/environment/env-metal-1.hdr" />
+        <OrbitControls
+          makeDefault
+          autoRotate
+          autoRotateSpeed={3}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2}
+          enableZoom={false}
+          enablePan={false}
         />
-      </EffectComposer>
-    </Canvas>
+        <EffectComposer multisampling={0} disableNormalPass={true}>
+          <Bloom
+            luminanceThreshold={1}
+            luminanceSmoothing={0.9}
+            intensity={intensity}
+            levels={level}
+            mipmapBlur
+          />
+        </EffectComposer>
+      </Canvas>
+      <Loader containerStyles={containerStyles} dataStyles={textStyles} />
+    </>
   );
 };
 
