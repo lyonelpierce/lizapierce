105a376dbb5f6982dc09f70782cd54de28ec1798
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2024-01-10 14:57:26 -0500

    footer icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,24 @@ import Link from "next/link";
 import { Facebook, Instagram, Twitter } from "lucide-react";
 
 const Footer = () => {
+  const socialItems = [
+    {
+      name: "Facebook",
+      link: "/",
+      icon: <Facebook className="w-4 h-4" fill="bg-zinc-900" strokeWidth={1} />,
+    },
+    {
+      name: "Instagram",
+      link: "/",
+      icon: <Instagram className="w-4 h-4" fill="bg-zinc-900" stroke="white" />,
+    },
+    {
+      name: "Twitter",
+      link: "/",
+      icon: <Twitter className="w-4 h-4" fill="bg-zinc-900" strokeWidth={1} />,
+    },
+  ];
+
   return (
     <div className="relative flex items-center w-full h-12 text-xs bg-zinc-900">
       <div className="max-w-7xl mx-auto w-full px-4">
@@ -11,33 +29,13 @@ const Footer = () => {
             <h1 className="text-xs font-medium uppercase">Eliza Pierce</h1>
           </Link>
           <ul className="flex gap-3">
-            <li className="bg-white text-zinc-900 rounded-full p-1">
-              <Link href="/" target="_blank">
-                <Facebook
-                  className="w-5 h-5"
-                  fill="bg-zinc-900"
-                  strokeWidth={1}
-                />
-              </Link>
-            </li>
-            <li className="bg-white text-zinc-900 rounded-full p-1">
-              <Link href="/" target="_blank">
-                <Instagram
-                  className="w-5 h-5"
-                  fill="bg-zinc-900"
-                  stroke="white"
-                />
-              </Link>
-            </li>
-            <li className="bg-white text-zinc-900 rounded-full p-1">
-              <Link href="/" target="_blank">
-                <Twitter
-                  className="w-5 h-5"
-                  fill="bg-zinc-900"
-                  strokeWidth={1}
-                />
-              </Link>
-            </li>
+            {socialItems.map((item) => (
+              <li className="bg-white text-zinc-900 rounded-full p-1 transition-transform ease-in-out hover:scale-125 duration-300">
+                <Link href={item.link} target="_blank">
+                  {item.icon}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
